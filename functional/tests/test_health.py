39138ee8db004b128f022e8c07a34c627e384848
AMirshekari <amirshekari@ebates.com>
2020-02-04 14:20:49 -0800

    Some Update

--- functional/tests/test_health.py
+++ functional/tests/test_health.py
@@ -1,9 +1,14 @@
 import pytest
 
 
-@pytest.mark.health_test
 class TestHealth:
 
+    @pytest.mark.health_test
     def test_health(self):
+        is_healthy = True
+        assert is_healthy
+
+    @pytest.mark.health_test
+    def test_health_2(self):
         is_healthy = True
         assert is_healthy
